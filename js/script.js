1cecd22d902dba807b92e7a95e468b7065fd857f
dekyNino <60123559+dekyNino@users.noreply.github.com>
2021-11-09 22:25:49 +0800

    First commit
    First commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+// MODAL START
+$('#galleryModal').on('show.bs.modal', function (e) {
+    $('#galleryImage').attr("src",$(e.relatedTarget).data("large-src"));
+ });
