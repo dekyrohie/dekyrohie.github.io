5695f835f56cf97fc2d70246ed0723bdd203cceb
dekyNino <60123559+dekyNino@users.noreply.github.com>
2021-11-12 22:37:14 +0800

    commit 1
    commit 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-$(function () {
+// MODAL START
+$('#galleryModal').on('show.bs.modal', function (e) {
+    $('#galleryImage').attr("src",$(e.relatedTarget).data("large-src"));
+ });
+
+//  TRANSPARENT NAVBAR
+ $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('active');
@@ -7,11 +13,3 @@ $(function () {
         }
     });
 });
-
-// MODAL START
-$('#galleryModal').on('show.bs.modal', function (e) {
-    $('#galleryImage').attr("src",$(e.relatedTarget).data("large-src"));
- });
-
-//  SCROLL
-var scroll = new SmoothScroll('a[href*="#_"]');
